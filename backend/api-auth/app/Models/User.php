9c07ddd9df9b12491441b6c8d4f08537cab43298
Irashe0 <iratxe.pastana@juanxxiii.net>
2025-02-04 20:49:45 +0100

    q sigue funcioanandoooo

--- backend/api-auth/app/Models/User.php
+++ backend/api-auth/app/Models/User.php
@@ -9,6 +9,8 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * Class User
@@ -29,6 +31,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class User extends Model
 {
+	use HasFactory, HasApiTokens;
 	protected $table = 'users';
 
 	protected $casts = [
