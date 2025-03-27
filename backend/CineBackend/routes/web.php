c68533f5bc82f6152fc2af9e8c9d60736e7a37b4
Irashe0 <iratxe.pastana@juanxxiii.net>
2025-03-27 20:39:15 +0100

    eeeee creo que el back ya esta hecho

--- backend/CineBackend/routes/web.php
+++ backend/CineBackend/routes/web.php
@@ -1,20 +1,21 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Ruta principal que carga React
+Route::get('/{any}', function () {
+    return File::get(public_path('dist/index.html'));
+})->where('any', '.*');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
+// Rutas protegidas con autenticación
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Cargar las rutas de autenticación
 require __DIR__.'/auth.php';
